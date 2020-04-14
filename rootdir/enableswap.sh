bebd7ab9935fb5a1b32a28cf6a1f79edaa1b0939
muralivijay <muralidharan9845@gmail.com>
2020-04-15 00:37:37 +0530

    Q4601: Enable zram support
    * 512mb enough.
    * enable full swapiness level.

--- rootdir/enableswap.sh
+++ rootdir/enableswap.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-echo 0 > /sys/block/zram0/disksize
-/system/bin/mkswap /dev/block/zram0
-/system/bin/swapon /dev/block/zram0
+#echo 0 > /sys/block/zram0/disksize
+#/system/bin/mkswap /dev/block/zram0
+#/system/bin/swapon /dev/block/zram0
+
+# Set swappiness
+echo 100 > /proc/sys/vm/swappiness
