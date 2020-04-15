9384d1271a85c45527c318120a449a01bedfb464
muralivijay <muralidharan9845@gmail.com>
2020-04-15 18:25:14 +0530

    Q4601: Fix zram
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- rootdir/enableswap.sh
+++ rootdir/enableswap.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-#echo 0 > /sys/block/zram0/disksize
-#/system/bin/mkswap /dev/block/zram0
-#/system/bin/swapon /dev/block/zram0
 
-# Set swappiness
+# ZRAM swap by fei.liu@tinno.com @Apr 1
+echo 536870912 > /sys/block/zram0/disksize
+/system/bin/mkswap /dev/block/zram0
+/system/bin/swapon /dev/block/zram0
+
+# Set swappiness by fei.liu@tinno.com @Apr 1
 echo 100 > /proc/sys/vm/swappiness
+
