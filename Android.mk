649682bae03370d45a4f80eb8523f058bbf6cdf9
muralivijay <muralidharan9845@gmail.com>
2020-04-15 18:34:04 +0530

    Q4601: HACK around KERNEL_OBJ because of using prebuilt kernel

--- Android.mk
+++ Android.mk
@@ -2,6 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),q4601)
 
+# HACK for prebuilt kernel
+$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
+$(shell touch $(OUT)/obj/KERNEL_OBJ/usr/export_includes)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
